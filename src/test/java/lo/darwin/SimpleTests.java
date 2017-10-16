09a702b928b43c4261410d234b6b1227829bab40
Darwin Lo <darwinlo@winkhealth.com>
2017-10-16 11:19:19 -0700

    Exercised maps.

--- src/test/java/lo/darwin/SimpleTests.java
+++ src/test/java/lo/darwin/SimpleTests.java
@@ -2,6 +2,7 @@ package lo.darwin;
 
 import java.util.*;
 
+import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -18,7 +19,7 @@ class SimpleTests {
   }
 
   @Test
-  void testCollections(TestInfo testInfo) {
+  void testSets(TestInfo testInfo) {
     Set<Integer> numbers = new HashSet<>(); // using the diamond operator
     numbers.add(Integer.valueOf(3)); // without auto-boxing
     numbers.add(4); // with auto-boxing
@@ -27,5 +28,19 @@ class SimpleTests {
     assertTrue(numbers.contains(4));
     assertTrue(numbers.containsAll(Set.of(3, 4))); // of creates an immutable set
     assertFalse(numbers.contains(5));
+  }
+
+  @Test
+  void testMaps(TestInfo testInfo) {
+    Map<String, Integer> accountBalances = new HashMap<>();
+    accountBalances.put("Darwin Lo", 100);
+    assertEquals(accountBalances.get("Darwin Lo"), Integer.valueOf(100),
+      "The account balance for Darwin is not $100.");
+
+    int removed = accountBalances.remove("Darwin Lo"); // auto-unboxing
+    assertEquals(removed, 100, "The removed value should be 100.");
+
+    assertFalse(accountBalances.containsKey("Darwin Lo"),
+      "\"Darwin Lo\" should've been removed.");
   }
 }
