26e38bc50c781939c3a040e674409d172afc357b
Darwin Lo <darwinlo@winkhealth.com>
2017-10-15 22:28:30 -0700

    Added a unit test to exercise functionality of sets.

--- src/test/java/lo/darwin/SimpleTests.java
+++ src/test/java/lo/darwin/SimpleTests.java
@@ -1,6 +1,10 @@
 package lo.darwin;
 
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,5 +15,17 @@ class SimpleTests {
   @DisplayName("Simple test")
   void simple(TestInfo testInfo) {
     assertEquals(1, 1, "1 should equal 1.");
+  }
+
+  @Test
+  void testCollections(TestInfo testInfo) {
+    Set<Integer> numbers = new HashSet<>(); // using the diamond operator
+    numbers.add(Integer.valueOf(3)); // without auto-boxing
+    numbers.add(4); // with auto-boxing
+
+    assertTrue(numbers.contains(3));
+    assertTrue(numbers.contains(4));
+    assertTrue(numbers.containsAll(Set.of(3, 4))); // of creates an immutable set
+    assertFalse(numbers.contains(5));
   }
 }
